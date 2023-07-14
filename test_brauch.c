97f1850005a156b72aadd9fd3613126bac5c0254
serhiiMiauen <138523529+serhiiMiauen@users.noreply.github.com>
2023-07-14 23:48:57 +0200

    Update test_brauch.c

--- test_brauch.c
+++ test_brauch.c
@@ -5,10 +5,10 @@
 
 void login (void);
 void registration(void);
-void user_log_in(void);
+void user_log_in(char username[40]);
 void to_rent_car(void);
 void current_auto(void);
-void display_user_data(void);
+void display_user_data(char username[40]);
 
 
 struct login
@@ -44,6 +44,8 @@ void registration (void){
     struct login j;
     fgets(j.fname, sizeof(j.fname), stdin);    
 
+    int x = 0;
+    while (x != 9){
     printf("Welcome to the car rental service. To beggin using this app, you need to give some details\n Please, enter your First Name: \n");
     fgets(j.fname, sizeof(j.fname), stdin);    
     printf("And now, Please enter your Surname\n");
@@ -55,12 +57,18 @@ void registration (void){
     printf("Now enter your IBAN\n");
     fgets(j.IBAN, sizeof(j.IBAN), stdin);    
 
-    printf("We almost finished, now enter username\n");
+    printf("Now enter username\n");
     fgets(j.username, sizeof(j.username), stdin);    
 
     printf("Enter your password\n");
     fgets(j.password, sizeof(j.password), stdin);    
 
+    if (strcmp(j.fname, "\n") == 0 || strcmp(j.lname, "\n") == 0 || strcmp(j.passport_id_number, "\n") == 0 || strcmp(j.IBAN, "\n") == 0 || strcmp(j.username, "\n") == 0 || strcmp(j.password, "\n") == 0) 
+    {
+        printf("There is a mistake. Looks like you missed a line\nLet's try again\n\n");
+    }
+    else {break;}
+    }
     fwrite(&j, sizeof(j), 1, user_data);
     fclose(user_data);
 
@@ -98,7 +106,7 @@ void login (void){
 
     if (found == 1){
         printf("\nSuccessful login\n");
-        user_log_in();
+        user_log_in(username);
     } 
     else{
         printf("Incorrect login details\n");
@@ -107,7 +115,7 @@ void login (void){
 }
 
 
-void user_log_in(void){
+void user_log_in(char username[40]){
     int i = 1;
     while (i != 0){
     int option = 0;
@@ -121,7 +129,7 @@ void user_log_in(void){
             current_auto();
             break;
         case 3: 
-            display_user_data();
+            display_user_data(username);
             break;
         case 9:
             exit(1);
@@ -136,4 +144,41 @@ void user_log_in(void){
 }
 void to_rent_car(void){printf("a");}
 void current_auto(void){printf("b");}
-void display_user_data(void){printf("c");}
+
+
+void display_user_data(char username[40]){
+
+    FILE *user_data;
+    user_data = fopen("user_data.txt", "r");    
+    if (user_data == NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    struct login l;
+    int found = 0; 
+
+    while (fread (&l, sizeof(l), 1, user_data)){
+        if (strcmp(username, l.username) == 0){
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 1){
+        printf("First name: %s", l.fname);
+        printf("Lase name: %s", l.lname);
+        printf("Username: %s", l.username);
+        printf("IBAN: %s", l.IBAN);
+        printf("Password: %s\n", l.password);
+        
+    } 
+    else{
+        printf("Sorry, your details were not found. Please contact technical support");
+    }
+    fclose(user_data);
+    printf("Press any key to continue\n");
+    getchar();
+}
+
+
